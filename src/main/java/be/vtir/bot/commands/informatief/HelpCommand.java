2c1dff32f3c9165558a4dfb8749a313820ee858d
Marijn2341 <marijn1232@hotmail.com>
2021-04-25 21:37:36 +0200

    Help Command Updated

--- src/main/java/be/vtir/bot/commands/informatief/HelpCommand.java
+++ src/main/java/be/vtir/bot/commands/informatief/HelpCommand.java
@@ -11,11 +11,13 @@ public class HelpCommand extends Command {
     @Override
     public void onCommand(Command cmd, String[] args, Message msg) {
        msg.getChannel().sendMessage("**Help Pagina**\n```Botinfo || Krijg info over de discord bot.\n" +
-               "Members || Zie hoeveel members er in de server zitten.\n" +
-               "Uptime || Zie wat de uptime van de bot is.\n" +
-               "8ball || Stel een vraag aan de bot.\n" +
-               "Avatar || Vergroot jou profielfoto of die van iemand anders.\n" +
-               "Meme || Laat de bot een meme plaatsen.\n" +
-               "Userinfo || Krijg informatie te zien over een bepaalde gebruiker.```").queue();
+               "!Members || Zie hoeveel members er in de server zitten.\n" +
+               "!Uptime || Zie wat de uptime van de bot is.\n" +
+               "!Ask || Stel een vraag aan de bot.\n" +
+               "!Avatar || Vergroot jou profielfoto of die van iemand anders.\n" +
+               "!Meme || Laat de bot een meme plaatsen.\n" +
+               "!Userinfo || Krijg informatie te zien over een bepaalde gebruiker.\n" +
+               "!Coinflip || Speel kop of munt.\n" +
+               "!Mop || Laat de bot een mop plaatsen.```").queue();
     }
 }
